974ceaa3bbc0f275771c0d14bf2d037537541c11
Tyrell <tysteg14@gmail.com>
2023-08-25 14:27:15 -0500

    fetch requests

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Add your code here
+function submitData(name, email) {
+    const url = "http://localhost:3000/users";
+  
+    const userData = {
+      name: name,
+      email: email
+    };
+  
+    const configurationObject = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(userData)
+    };
+  
+    return fetch(url, configurationObject)
+      .then(response => response.json())
+      .then(data => {
+        const newId = data.id;
+        const newIdElement = document.createElement("p");
+        newIdElement.textContent = `New ID: ${newId}`;
+        document.body.appendChild(newIdElement);
+      })
+      .catch(error => {
+        const errorMessageElement = document.createElement("p");
+        errorMessageElement.textContent = error.message;
+        document.body.appendChild(errorMessageElement);
+      });
+  }
+
